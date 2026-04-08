938d955e30e7eda7395c77da5566b82e4ad5a2b1
Dell <himpreet252@gmail.com>
2026-04-08 11:38:10 +0530

    pickup controller on, pre save pickup

--- lib/controller/authController.dart
+++ lib/controller/authController.dart
@@ -1320,7 +1320,10 @@ class AuthController extends GetxController implements GetxService
 
   }
 
-
+  String pickupAddressMultiLocation = '';
+  String pickupAddressMultiLocationLat = '';
+  String pickupAddressMultiLocationLng = '';
+  String pickupAddressMultiLocationCity = '';
   GetLastFiveDropLocationsModel getLastFiveDropLocationsModel = GetLastFiveDropLocationsModel();
 
   Future<void>getLastFiveDrop({String? userId})
